efd0dbee4cc66b9741973e330b56460882d3f28e
Dav Clark <dav@Rain.local>
2010-01-08 17:24:38 -0800

    added a copy of the plot_params.py file

---
+++ plot_params.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+# Standard libs
+import re
+from warnings import warn, simplefilter
+
+
+# Third party libs
+import argparse
+
+import numpy as np
+from enthought.enable.api import ComponentEditor
+import enthought.traits.api as traits
+import enthought.traits.ui.api as ui
+import enthought.chaco.api as chaco
+import enthought.chaco.tools.api as tools
+
+PARAM_DTYPE = [('rot', [('x', 'f4'), ('y', 'f4'), ('z', 'f4')]), 
+               ('trans', [('x', 'f4'), ('y', 'f4'), ('z', 'f4')])]
+
+# cmap = chaco.Spectral(chaco.DataRange1D(low=0, high=5))
+# COLOR_PALETTE = cmap.color_bands
+
+
+class MovementParamPlot(traits.HasTraits):
+    rot_plot = traits.Instance(chaco.Plot)
+    trans_plot = traits.Instance(chaco.Plot)
+    data = traits.DictStrAny(value_trait=chaco.ArrayPlotData)
+    zoom = traits.Instance(tools.ZoomTool)
+    pan = traits.Instance(tools.PanTool)
+    # It'd be nice to split out the notion of which subject is selected, so that
+    # this plot can simply take a given set of data
+    max_params = traits.Int(0)
+    params_num = traits.Range(0, 'max_params') 
+
+    traits_view = ui.View( 
+                    ui.Item('params_num', label="Scan Number",
+                            editor=ui.RangeEditor(mode='spinner',
+                                                  high_name='max_params') ),
+                    ui.Item('rot_plot', label='Rotation', 
+                            editor=ComponentEditor()),
+                    ui.Item('trans_plot', label='Translation', 
+                            editor=ComponentEditor()),
+                    # width=550, height=550, 
+                    resizable=True, 
+                    title="Movement parameters"
+                  )
+    
+    def __init__(self, params):
+        '''params: a list of numpy arrays'''
+        super(MovementParamPlot, self).__init__()
+
+        self.params_num = 0
+        self.max_params = len(params) - 1
+
+        self.params = params
+
+
+        # XXX - the following info should go (soon) into 
+        # nipype.interfaces.fsl.McFLIRT
+        self.trans_plot = self.create_line_plot('trans', 'mm')
+        self.rot_plot = self.create_line_plot('rot', 'radians (clockwise)',
+                                              self.trans_plot.index_range)
+
+        self.zoom = tools.ZoomTool(self.trans_plot, tool_mode='range', 
+                                   axis='index')
+        self.pan = tools.PanTool(self.rot_plot, constrain=True,
+                                 constrain_direction='x')
+        self.trans_plot.tools.extend((self.zoom, self.pan))
+        self.rot_plot.tools.extend((self.pan, self.zoom))
+
+
+    def create_line_plot(self, name, units='Arbitrary Units', 
+                         index_range=None):
+        '''Make a line plot overlaying x, y and z values'''
+        data = chaco.ArrayPlotData()
+        plot = chaco.Plot(data)
+        if index_range is not None:
+            plot.index_range = index_range
+
+        xyz = self.params[0][name]
+        axes = xyz.dtype.names
+        for axis in axes:
+            data.set_data(axis, xyz[axis])
+            plot.plot(axis, color='auto')
+
+        plot.index_axis.title = 'Slice Number'
+        plot.value_axis.title = units
+        # Our left axis labels are a bit large
+        plot.padding = [70, 50, 20, 50]
+        # plot.tools.extend((self.zoom, self.pan))
+
+        # legend = chaco.Legend(component=plot, padding=10, align="ur")
+        plot.legend.tools.append(
+                tools.LegendTool(plot.legend, drag_button="right") )
+        # plot.overlays.append(legend)
+        plot.legend.labels = list(axes)
+        plot.legend.visible = True
+
+        self.data[name] = data
+
+        return plot
+
+    # TODO: need to re-set range on plots
+    @traits.on_trait_change('params_num')
+    def set_plot(self, n=None):
+        if n is None:
+            n = self.params_num
+        else:
+            self.params_num = n
+
+        params = self.params[n]
+        for name in params.dtype.names:
+            xyz = params[name]
+            for axis in xyz.dtype.names:
+                self.data[name].set_data(axis, xyz[axis])
+
+class TimediffPlot(traits.HasTraits):
+    traits_view = ui.View( 
+                    ui.Item('params_num', label="Scan Number",
+                            editor=ui.RangeEditor(mode='spinner',
+                                                  high_name='max_params') ),
+                    ui.Item('rot_plot', label='Rotation', 
+                            editor=ComponentEditor()),
+                    ui.Item('trans_plot', label='Translation', 
+                            editor=ComponentEditor()),
+                    # width=550, height=550, 
+                    resizable=True, 
+                    title="Movement parameters"
+                  )
+       
+class PhysioPlot(traits.HasTraits):
+    traits_view = ui.View( 
+                    ui.Item('params_num', label="Scan Number",
+                            editor=ui.RangeEditor(mode='spinner',
+                                                  high_name='max_params') ),
+                    ui.Item('rot_plot', label='Rotation', 
+                            editor=ComponentEditor()),
+                    ui.Item('trans_plot', label='Translation', 
+                            editor=ComponentEditor()),
+                    # width=550, height=550, 
+                    resizable=True, 
+                    title="Movement parameters"
+                  )
+def main(fnames, verbose=False, plot=True):
+    '''main(fnames): simply reads each file in fnames'''
+    if verbose:
+        simplefilter('always', UserWarning)
+    else:
+        simplefilter('ignore', UserWarning)
+
+    # Allow passing in a single string
+    if isinstance(fnames, str):
+        fnames = [fnames]
+
+    params = []
+    for fname in fnames:
+        params.append(np.recfromtxt(fname, dtype=PARAM_DTYPE))
+
+    plot_obj = MovementParamPlot(params)
+    plot_obj.configure_traits()
+    return plot_obj
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='store_true') 
+    parser.add_argument('fnames', nargs='+')
+    args = parser.parse_args()
+    main(args.fnames, verbose=args.verbose, plot=False)
